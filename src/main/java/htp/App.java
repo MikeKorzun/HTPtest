302a4eb56d1f6e44b807b233af032dd8204ee545
Mike Korzun <mikebagoria@gmail.com>
2017-11-17 19:06:47 +0300

    #3_MainApp Was realized logic of starting dancing and returning the result in console

--- src/main/java/htp/App.java
+++ src/main/java/htp/App.java
@@ -1,12 +1,40 @@
 package htp;
 
+import htp.Entity.DanceHall;
+import htp.Entity.Dancer;
+
+
 import java.io.IOException;
 
 
-
-public class App
+/**
+ *
+ * Стартовая позиция для танца = первая цифра в последовательности = начало площадки
+ * Танец окончен, если вышли за пределы площадки
+ * Танец вечный, если мы не можем выйти за пределы площадки
+ * Числовая последовательность должна содержать цифры, разделенные пробелом,
+ * количество чисел от 1 до 50, все числа <0 будут преобразованы в положительные
+ */
+public class App 
 {
     public static void main( String[] args ) throws IOException {
-
+        DanceHall danceHall = new DanceHall();
+        Dancer dancer = new Dancer();
+        // OK moving
+        while (dancer.canMove(
+                dancer.getPosition(),
+                danceHall.getArea().get(dancer.getPosition()),
+                danceHall.getArea().size())
+        ) {
+            if (!danceHall.isAreaInfinite()) {
+                dancer.move(danceHall.getArea().get(dancer.getPosition()), danceHall.getArea().size());
+                if (dancer.getPosition()>=danceHall.getArea().size()) break;
+                if (dancer.getPosition()<0) break;
+            } else {
+                dancer.setTotalSteps(-1);
+                break;
+            }
+        }
+        System.out.println(dancer.getTotalSteps());
     }
-}
+ }
